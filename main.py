4c6079654f7a733aee61be4abb3b1dd421d84207
ktschanz <keltentschanz@gmail.com>
2021-07-08 10:01:48 -0600

    updates

--- main.py
+++ main.py
@@ -34,11 +34,11 @@ async def on_message(message):
     if '!treeteller' in message.content.lower():
         tree = trees[random.randint(0, len(trees))]
 
-        name = tree[1] if tree[1] != '' else "Unknown"
-        species = tree[2] if tree[2] != '' else "Unknown"
-        age = tree[3] if tree[3] != '' else "Unknown"
-        location = tree[4] if tree[4] != '' else "Unknown"
-        description = tree[5] if tree[5] != '' else "Unknown"
+        name = tree[1] if tree[1] != '' and tree[1] != '-' else "Unknown"
+        species = tree[2] if tree[2] != '' and tree[2] != '-' else "Unknown"
+        age = tree[3] if tree[3] != '' and tree[3] != '-' else "Unknown"
+        location = tree[4] if tree[4] != '' and tree[4] != '-' else "Unknown"
+        description = tree[5] if tree[5] != '' and tree[5] != '-' else "Unknown"
 
         embed = discord.Embed(title=name, colour=discord.Colour.random())
         embed.add_field(name="Species", value=species, inline=False)
